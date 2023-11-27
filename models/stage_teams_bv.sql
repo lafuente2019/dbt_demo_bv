b7ee7c2d6ac0d69bf10ab8cf50b563bb57d5fe50
Valter Lafuente Junior <valterlafuentejunior@gmail.com>
2023-11-27 14:15:06 +0000

    Alteração no arquivo stage_teams_bv

--- models/stage_teams_bv.sql
+++ models/stage_teams_bv.sql
@@ -1,13 +1,15 @@
-with teams_bv as (
+with time_bv as (
     SELECT * FROM {{ref('teams_bv')}}
 )
 SELECT 
 *,
 CASE
-WHEN time = 'Palmeiras' THEN 'Orgulhoso'
-WHEN time = 'São Paulo' THEN 'Ferias'
-WHEN time = 'Corinthians' THEN 'Otimista'
-WHEN time = 'Santos' THEN 'Incaivel'
-WHEN time = 'Flamengo' THEN 'Vergonhoso'
+WHEN time = 'Palmeiras' THEN 'Orgulhosos'
+WHEN time = 'São Paulo' THEN ''
+WHEN time = 'Corinthians' THEN 'Desesperados'
+WHEN time = 'Cruzeiro' THEN 'Desesperados'
+WHEN time = 'Internacional' THEN 'Desesperados'
+WHEN time = 'Nenhum' THEN 'Decidiram não sofrer'
+WHEN time = 'Flamengo' THEN 'Frustados'
 END AS Status_teams
-FROM teams_bv
+FROM time_bv
